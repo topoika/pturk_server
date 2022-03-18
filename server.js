30b6408159ae3e53256738f127c4f9b84921e47c
Topoika Tampul <77112717+topoika@users.noreply.github.com>
2022-03-18 18:17:46 +0300

    Update server.js

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ const db = mysql.createPool({
 });
 
 const app = express();
-const PORT = process.env.LOCAL_PORT | 3000;
+const PORT = process.env.LOCAL_PORT || 3000;
 app.listen(PORT, () => {
   console.log("Server is running");
   // firstQuery();
